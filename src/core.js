705322d16cfcf17789d9db44331d45e2e375b959
Samson Brock <SamsonLB@gmail.com>
2016-04-23 14:16:41 -0700

    Complete core app logic with Immutable specs.
    The core logic of the app is now complete with unit tests written using
    the Immutable data structure library.

--- src/core.js
+++ src/core.js
@@ -1,16 +1,45 @@
 import {List, Map} from 'immutable'
 
+function getWinners (vote) {
+  if (!vote) return []
+
+  const [a, b] = vote.get('pair')
+  const aVotes = vote.getIn(['tally', a], 0)
+  const bVotes = vote.getIn(['tally', b], 0)
+
+  if (aVotes > bVotes) {
+    return [a]
+  } else if (bVotes > aVotes) {
+    return [b]
+  } else {
+    return [a, b]
+  }
+}
+
 export function setEntries (state, entries) {
   return state.set('entries', List(entries))
 }
 
 export function next (state) {
-  const entries = state.get('entries')
+  const entries = state.get('entries').concat(getWinners(state.get('vote')))
 
-  return state.merge({
-    vote: Map({ pair: entries.take(2) }),
-    entries: entries.skip(2)
-  })
+  if (entries.size === 1) {
+    // Map({ winner: entries.first() }) could have been returned instead, but it
+    // is better to be more explicit about state changes. This has the added
+    // benefit of reducing the likelihood of errors should state contain other
+    // data in the future.
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first())
+  } else {
+    // The merge below did not work exactly like I thought it might.
+    // If the original state has a tally under vote, the tally will be lost
+    // because the vote containing just the pair will replace the original vote.
+    return state.merge({
+      vote: Map({ pair: entries.take(2) }),
+      entries: entries.skip(2)
+    })
+  }
 }
 
 export function vote (state, entry) {
