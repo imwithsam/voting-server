6cde74436127c168b76dec1d3949847a4131915f
Samson Brock <SamsonLB@gmail.com>
2016-04-25 11:36:53 -0700

    Broadcast state from a Redux listener.
    Add a callback to the Redux Store subscription so WebSockets will emit
    the current app state to all clients whenever an action is applied.
    Also, have the Socket.io Server publish the current app state to clients
    when they connect.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 import Server from 'socket.io'
 
-export default function startServer() {
+export default function startServer (store) {
   const io = new Server().attach(8090)
+
+  // Below, the whole app state is being published to all clients. Future
+  // optimization may be to emit only relevant state subsets or diffs.
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  )
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS())
+  })
 }
